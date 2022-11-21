a656781f708ab25f69f98a16e3e242424d0bd096
sonwjn111 <104009123+sonchymto1@users.noreply.github.com>
2022-11-21 11:33:43 +0700

    Initial simple data.

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { isEmpty } from 'lodash'
+
 import './BoardContent.scss'
 import BoardItem from 'components/BoardItem/BoardItem'
-
+import { initialData } from 'actions/initialData'
+import { mapOrder } from 'utilities/sorts'
 function BoardContent() {
+  const [board, setBoard] = useState({})
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+    if (boardFromDB) {
+      setBoard(boardFromDB)
+      setItems(mapOrder(boardFromDB.items, boardFromDB.itemOrder, 'id'))
+    }
+  }, [])
+
+  if (isEmpty(board)) {
+    return <div className="not-board">No Board Content!</div>
+  }
+
   return (
     <div className="board-content">
-      <BoardItem />
-      <BoardItem />
-      <BoardItem />
+      {items.map((item, index) => (
+        <BoardItem item={item} key={index} />
+      ))}
     </div>
   )
 }
