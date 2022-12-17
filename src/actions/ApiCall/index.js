d69b763f7dbbbf04883db9c3dcb4efc7b2f4f1ff
sonwjn111 <104009123+sonchymto1@users.noreply.github.com>
2022-12-17 16:06:35 +0700

    drag drop card column.

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -6,18 +6,28 @@ export const fetchBoardDetails = async boardId => {
   return request.data
 }
 
+export const updateBoard = async (boardId, data) => {
+  const request = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, data)
+  return request.data
+}
+
 export const createNewColumn = async data => {
   const request = await axios.post(`${API_ROOT}/v1/columns`, data)
   return request.data
 }
 
 //update or remove column
-export const updateColumn = async (id, data) => {
-  const request = await axios.put(`${API_ROOT}/v1/columns/${id}`, data)
+export const updateColumn = async (columnId, data) => {
+  const request = await axios.put(`${API_ROOT}/v1/columns/${columnId}`, data)
   return request.data
 }
 
 export const createNewCard = async data => {
   const request = await axios.post(`${API_ROOT}/v1/cards`, data)
+  return request.data
+}
+
+export const updateCard = async (cardId, data) => {
+  const request = await axios.put(`${API_ROOT}/v1/cards/${cardId}`, data)
   return request.data
 }
