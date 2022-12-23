afa9cfb946270d04ab9d7618eeaf9f291c5adb38
sonwjn111 <104009123+sonchymto1@users.noreply.github.com>
2022-12-23 15:41:30 +0700

    deploy commit to railway.

--- src/utilities/contants.js
+++ src/utilities/contants.js
@@ -1,5 +1,5 @@
 // export const API_ROOT = 'http://localhost:5070'
-export const API_ROOT = 'trello-api-production.up.railway.app'
+export const API_ROOT = 'https://trello-api-production.up.railway.app'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
